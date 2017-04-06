e2c587a7b38620331a9b52a5d18698c9d6beb161
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-06 16:17:12 +0200

    kind of adds new task but like 4 times

--- todo.py
+++ todo.py
@@ -5,29 +5,6 @@ text = open("todo_app.txt", "r")
 todo_list = text.readlines()
 text.close()
 
-def print_todo_list():
-    if len(todo_list) == 0:
-        print("No todos for today, man!")
-    else:
-        for todo in range(len(todo_list) + 1):
-            print(str(todo) + " " + (todo_list[todo - 1]))
-
-def controller():
-    if sys.argv[1] == "-l":
-        print_todo_list()
-
-
-controller()
-
-
-
-
-#if len(arguments) == 0:
-#    print('')
-#else:
-	#if( arguments[0] == '-l' ):
-		#print('something', arguments[1])
-
 def print_help():
     help_info =    ["Python todo application",
                     "=======================",
@@ -39,3 +16,33 @@ def print_help():
 
     for content in help_info:
         print(content)
+
+def print_todo_list():
+    if len(todo_list) == 0:
+        print("No todos for today, man!")
+    else:
+        for todo in range(len(todo_list) + 1):
+            print(str(todo) + " " + (todo_list[todo - 1]))
+
+def controller():
+    if sys.argv[1] == "-l":
+        print_todo_list()
+    if sys.argv[1] == "-a":
+        add_new_task()
+    if sys.argv[1] == "-r":
+        remove_task()
+    if sys.argv[1] == "-c":
+        complete_task()
+    else:
+        print_help()
+
+def add_new_task():
+    #if sys.argv[1] == "-a" and sys.argv[2] == "":
+        new_todo = open("todo_app.txt", "a")
+        new = "go to sleep"
+        add_todo = new_todo.writelines(new)
+        new_todo.close()
+
+add_new_task()
+
+controller()
