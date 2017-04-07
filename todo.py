3320ce1b70b748429af1b9eff1ee9e27a0587017
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-07 10:13:12 +0200

    controller works, adding new task is possible

--- todo.py
+++ todo.py
@@ -22,24 +22,32 @@ def print_todo_list():
         print("No todos for today, man!")
     else:
         for todo in range(1, len(todo_list) + 1):
-            print(str(todo) + " " + (todo_list[todo - 1]))
+            print(str(todo) + " [ ] " + (todo_list[todo - 1]))
 
 def controller():
-    if sys.argv[1] == "-l":
-        print_todo_list()
-    if sys.argv[1] == "-a":
-        add_new_task()
-    if sys.argv[1] == "-r":
-        remove_task()
-    if sys.argv[1] == "-c":
-        complete_task()
-    elif len(sys.argv) == 0:
-        print_help()
+    if len(sys.argv) >= 2:
+        if sys.argv[1] == "-l":
+            print_todo_list()
+        if sys.argv[1] == "-a":
+            add_new_task()
+        if sys.argv[1] == "-r":
+            remove_task()
+        if sys.argv[1] == "-c":
+            complete_task()
+    else:
+        print_help() 
 
 def add_new_task():
+    if len(sys.argv) > 2:
         new_todo = open("todo_app.txt", "a")
-        new = sys.argv[2]
+        new = sys.argv[2] + "\n"
         add_todo = new_todo.writelines(new)
         new_todo.close()
+    else:
+        print("no new task added")
+
+def remove_task():
+    if len(sys.argv) > 2:
+        removing = open("todo_app.txt")
 
 controller()
